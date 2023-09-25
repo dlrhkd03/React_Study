25091e8dd79cdc7c9b5f9bf8c26a4b055f9ef0dc
이광호 사원 데브옵스파트 (lgHo@biztechi.com <slsl01@naver.com>
2023-09-25 17:31:00 +0900

    study_project

---
+++ 프로젝트/sleact-master/alecture/client.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from '@layouts/App';
+
+render(<App />, document.querySelector('#app'));
+
+// pages - 페이지
+// components - 작은 컴포넌트들
+// layouts - 공통 레이아웃
